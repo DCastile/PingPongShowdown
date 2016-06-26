a0dfb5d0862a5ea0a26dc41bf8f03546c386f121
dly9014 <young.danal@gmail.com>
2016-06-26 09:34:39 -0700

    clean ups

--- publishButtonClick.bat
+++
@@ -1,5 +0,0 @@
-del buttonClick.zip 
-cd lambda/buttonClick 
-7z a -r ../../buttonClick.zip * 
-cd ../.. 
-aws lambda update-function-code --function-name TennisMatchManager-Dev --zip-file fileb://buttonClick.zip
