69ab009048e5133874a902589a246afca2cdfc08
dly9014 <young.danal@gmail.com>
2016-07-16 15:12:11 -0700

    clean ups

--- publishCoreApp.bat
+++
@@ -1,5 +0,0 @@
-del coreApp.zip 
-cd lambda/coreApp 
-7z a -r ../../coreApp.zip * 
-cd ../.. 
-aws lambda update-function-code --function-name PingPongMatchManager-Dev --zip-file fileb://coreApp.zip
