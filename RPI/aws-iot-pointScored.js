c88cc9cff2e0762d5631b75ad78cb57b8147547b
dly9014 <young.danal@gmail.com>
2016-07-17 14:54:19 -0700

    add IoT parts

---
+++ RPI/aws-iot-pointScored.js
@@ -0,0 +1,40 @@
+var awsIot = require('aws-iot-device-sdk');
+var PythonShell = require('python-shell');
+
+var device = awsIot.device({
+   keyPath: './certs/private.pem.key',
+  certPath: './certs/certificate.pem.crt',
+    caPath: './certs/root-CA.crt',
+  clientId: 'RPI-2',
+    region: 'us-east-1'
+});
+ 
+device
+  .on('connect', function() {
+    console.log('connected');
+    device.subscribe({'iotbutton/G030JF056407W2QN':0}, function(error, result) {
+      console.log(result);
+    });
+  });
+  
+device
+  .on('message', function(topic, payload) {
+	var parsedPayload = JSON.parse(payload);
+	console.log('message topic:', topic);
+    console.log('message payload:', payload.toString());
+	console.log('click type = ' + parsedPayload.clickType);
+	
+	if (parsedPayload.clickType == 'SINGLE') {
+		// run the pointRed shell script
+		PythonShell.run('pointRed.py', function (err) {
+		  if (err) throw err;
+		});
+		
+	} else if (parsedPayload.clickType == 'DOUBLE') {
+		// run the pointBlue shell script
+		PythonShell.run('pointBlue.py', function (err) {
+		  if (err) throw err;
+		});		
+	}	
+});
+ 
---
+++ RPI/flic-iot-pointScored.js
@@ -0,0 +1,60 @@
+
+var PythonShell = require('python-shell');
+var fliclib = require("./fliclibNodeJs");
+var FlicClient = fliclib.FlicClient;
+var FlicConnectionChannel = fliclib.FlicConnectionChannel;
+var FlicScanner = fliclib.FlicScanner;
+
+var client = new FlicClient("localhost", 5551);
+
+function listenToButton(bdAddr) {
+		var cc = new FlicConnectionChannel(bdAddr);
+		client.addConnectionChannel(cc);
+		cc.on("buttonSingleOrDoubleClickOrHold", function(clickType, wasQueued, timeDiff) {
+				console.log(bdAddr + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
+				if (clickType == 'ButtonSingleClick' && bdAddr == '80:e4:da:71:30:f0'){
+					// run the pointRed shell script
+					PythonShell.run('pointRed.py', function (err) {
+					  if (err) throw err;
+					  console.log('black single clicked')
+					});
+						
+				} else if (clickType == 'ButtonDoubleClick' && bdAddr == '80:e4:da:71:30:f0'){
+					// run the pointBlue shell script
+					PythonShell.run('pointBlue.py', function (err) {
+					  if (err) throw err;
+					});	
+				} else{
+						// Longclick not used
+				}
+		});
+		cc.on("connectionStatusChanged", function(connectionStatus, disconnectReason) {
+				console.log(bdAddr + " " + connectionStatus + (connectionStatus == "Disconnected" ? " " + disconnectReason : ""));
+		});
+}
+
+client.once("ready", function() {
+	console.log("Connected to daemon!");
+	client.getInfo(function(info) {
+		info.bdAddrOfVerifiedButtons.forEach(function(bdAddr) {
+			listenToButton(bdAddr);
+		});
+	});
+});
+
+client.on("bluetoothControllerStateChange", function(state) {
+	console.log("Bluetooth controller state change: " + state);
+});
+
+client.on("newVerifiedButton", function(bdAddr) {
+	console.log("A new button was added: " + bdAddr);
+	listenToButton(bdAddr);
+});
+
+client.on("error", function(error) {
+	console.log("Daemon connection error: " + error);
+});
+
+client.on("close", function(hadError) {
+	console.log("Connection to daemon is now closed");
+});	
